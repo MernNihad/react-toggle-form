2d59346b47984dff48ea868792fa80800df01970
NIHAD O T <mernnihadsoft@gmail.com>
2023-07-13 09:13:39 +0530

    state with count

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+
+
+
+// useState
+// useEffect
+
 
 function App() {
+
+  const [state,setState] = useState(0);
+
+  
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign:"center"}}>
+
+
+    <h1>{state}</h1>
+    <button
+    
+    
+   onClick={
+
+    function name(params) {
+
+      console.log('button clicked')
+
+      setState(state+1)
+      
+    }
+
+
+   } 
+    
+    
+    
+    
+    >Increment +</button>
+
+
+
+
+    <button
+    
+    
+    
+    onClick={
+
+
+      function name(params) {
+        console.log('decrement...')
+        setState(state-1)
+      }
+
+    }
+    
+    
+    
+    >Descrement - </button>
+
     </div>
-  );
+    )
 }
 
-export default App;
+export default App
